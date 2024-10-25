19340fd74d56b864b58c012f71dcbb4dabf937dc
seon022 <seon022@icloud.com>
2024-10-25 14:09:40 +0900

    ✨ feat :  admin페이지 레이아웃 #6

--- clogging/src/app/admin/page.tsx
+++ clogging/src/app/admin/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+// import { Badge } from '@/components/ui/common/Badge';
+// import { Button } from '@/components/ui/common/Button';
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from '@/components/ui/common/Card';
+// import { useTheme } from '@/contexts/ThemeContext';
+
 export default function AdminPage() {
-  return <h1>admin</h1>;
+  return <div>sfdfsfsdf</div>;
 }
