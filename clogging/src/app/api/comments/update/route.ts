81ce83f853d31ba8d6e313610c66e0c3182c6a38
gd06070 <zizibae0715@naver.com>
2024-10-30 00:10:35 +0900

    🐛 Fix: 댓글 수정 API 수정 #30

--- clogging/src/app/api/comments/update/route.ts
+++ clogging/src/app/api/comments/update/route.ts
@@ -6,17 +6,22 @@ export async function PATCH(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const commentId = searchParams.get('commentId');
+    const postId = searchParams.get('postId');
     const { content, password } = await request.json();
 
-    if (!commentId || !content || !password) {
+    if (!commentId || !postId || !content || !password) {
       return NextResponse.json(
-        { error: 'commentId, content, password 중에 비어있는 값이 있음!' },
+        {
+          error:
+            'commentId, postId, content, password 중에 비어있는 값이 있음!',
+        },
         { status: 400 },
       );
     }
 
-    const commentRef = doc(db, 'comments', commentId);
+    const commentRef = doc(db, 'posts', postId, 'comments', commentId);
     const commentSnapshot = await getDoc(commentRef);
+
     if (!commentSnapshot.exists()) {
       return NextResponse.json(
         { error: '댓글 존재하지 않음!' },
@@ -34,7 +39,13 @@ export async function PATCH(request: Request) {
     return NextResponse.json(
       {
         message: '댓글 수정 성공!',
-        updatedComment: { id: commentId, content },
+        updatedComment: {
+          id: commentId,
+          postId,
+          content,
+          author: commentData.author,
+          createdAt: commentData.createdAt,
+        },
       },
       { status: 200 },
     );
