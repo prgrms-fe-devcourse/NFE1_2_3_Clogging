fb7a5165176f3f3c917860d016625eac0406c654
seon022 <seon022@icloud.com>
2024-10-29 14:42:42 +0900

    ✨ feat :  댓글관리 - 최근 댓글 데이터 반환 함수

--- clogging/src/features/Comment/ui/admin/CommentList.tsx
+++ clogging/src/features/Comment/ui/admin/CommentList.tsx
@@ -4,27 +4,34 @@ import React, { useState } from 'react';
 import { Comment } from '../../types';
 import CommentItem from './CommentItem';
 import EmptyComment from './EmptyComment';
+import { getMockComments } from '@/app/(auth)/admin/comment/page';
+import { Button } from '@/shared/ui/common/Button';
+
 interface CommentListProps {
-  comments: Comment[];
+  initialComments: Comment[];
 }
 
-const CommentList: React.FC<CommentListProps> = ({
-  comments: initialComments,
-}) => {
+const CommentList: React.FC<CommentListProps> = ({ initialComments }) => {
   const [comments, setComments] = useState(initialComments);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const { isDarkMode } = useTheme();
 
-  const handleDelete = async (id: string) => {
-    try {
-      // await deleteComment(id);
-      // 추후 수정.
-      setComments((prevComments) =>
-        prevComments.filter((comment) => comment.id !== id),
-      );
-      console.log(`댓글 ${id} 삭제됨`);
-    } catch (error) {
-      console.error('댓글 삭제 중 오류 발생:', error);
+  const handleLoadMore = () => {
+    const nextPage = page + 1;
+    const newComments = getMockComments(nextPage, 10);
+    if (newComments.length > 0) {
+      setComments((prev) => [...prev, ...newComments]);
+      setPage(nextPage);
+    } else {
+      setHasMore(false);
     }
+  };
+
+  const handleDelete = async (id: string) => {
+    setComments((prevComments) =>
+      prevComments.filter((comment) => comment.id !== id),
+    );
   };
 
   return (
@@ -34,15 +41,24 @@ const CommentList: React.FC<CommentListProps> = ({
       }`}
     >
       {comments.length > 0 ? (
-        <ul className="space-y-4">
-          {comments.map((comment) => (
-            <CommentItem
-              key={comment.id}
-              comment={comment}
-              onDelete={handleDelete}
-            />
-          ))}
-        </ul>
+        <>
+          <ul className="space-y-4">
+            {comments.map((comment) => (
+              <CommentItem
+                key={comment.id}
+                comment={comment}
+                onDelete={handleDelete}
+              />
+            ))}
+          </ul>
+          {hasMore && (
+            <div className="flex justify-center mt-6">
+              <Button onClick={handleLoadMore} variant="secondary">
+                더보기
+              </Button>
+            </div>
+          )}
+        </>
       ) : (
         <EmptyComment />
       )}
