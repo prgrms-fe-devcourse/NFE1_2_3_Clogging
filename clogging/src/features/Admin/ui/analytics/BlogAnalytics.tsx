0f65accfe72bdfc0ff0ca78dfaccd975fd96517c
seon022 <seon022@icloud.com>
2024-10-30 15:03:33 +0900

    ✨ Feat :  통계페이지 blog데이터 card ui #37

---
+++ clogging/src/features/Admin/ui/analytics/BlogAnalytics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BlogDataList from './BlogDataList';
+
+const BlogAnalytics = () => {
+  return (
+    <div>
+      <BlogDataList />
+    </div>
+  );
+};
+
+export default BlogAnalytics;
