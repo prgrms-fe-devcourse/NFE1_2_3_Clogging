ff1241b72396e8e68d50b9267bee09aca00cd5eb
seon022 <seon022@icloud.com>
2024-10-30 16:37:58 +0900

    ✨ Feat : 통계 페이지 recharts 적용

--- clogging/src/features/Admin/ui/analytics/BlogAnalytics.tsx
+++ clogging/src/features/Admin/ui/analytics/BlogAnalytics.tsx
@@ -1,10 +1,25 @@
+'use client';
 import React from 'react';
 import BlogDataList from './BlogDataList';
+import CustomLineChart from './LineChart';
+import { useTheme } from '@/shared/providers/theme';
 
 const BlogAnalytics = () => {
+  const { isDarkMode } = useTheme();
+  const data = [
+    { date: '12/11/2024', uv: 24, pv: 30 },
+    { date: '01/02/2023', uv: 31, pv: 28 },
+    { date: '01/03/2023', uv: 15, pv: 22 },
+    { date: '01/04/2023', uv: 28, pv: 19 },
+    { date: '01/05/2023', uv: 19, pv: 25 },
+  ];
+  const cardStyle = ` w-full md:w-1/2 ${isDarkMode ? 'bg-gray-900' : 'bg-white'}`;
   return (
     <div>
       <BlogDataList />
+      <div className={cardStyle}>
+        <CustomLineChart data={data} />
+      </div>
     </div>
   );
 };
---
+++ clogging/src/features/Admin/ui/analytics/LineChart.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip } from 'recharts';
+
+interface DataPoint {
+  date: string; // 날짜 형식
+  uv: number; // 값
+  pv: number; // 값
+}
+
+interface LineChartProps {
+  data: DataPoint[];
+}
+
+const CustomLineChart: React.FC<LineChartProps> = ({ data }) => (
+  <LineChart
+    width={'100%'} // Fill parent container
+    height={200}
+    data={data}
+    margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+  >
+    <CartesianGrid strokeDasharray="3 3" stroke="#e0e0e0" />
+    <XAxis dataKey="date" />
+    <Tooltip />
+    <CartesianGrid stroke="#e0e0e0" />
+    <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+    <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+  </LineChart>
+);
+
+export default CustomLineChart;
