2b64379792eba2d5fb5796487436d2b8f0e0410e
seon022 <seon022@icloud.com>
2024-11-05 00:24:05 +0900

    ✨ Feat : admin메인 - 통계카드 컴포넌트 적용

--- clogging/src/app/(auth)/admin/page.tsx
+++ clogging/src/app/(auth)/admin/page.tsx
@@ -1,45 +1,18 @@
 // 관리자 전용 페이지들
 
 import { Metadata } from 'next';
-import { Card, CardContent } from '@/shared/ui/common/Card';
-import { cn } from '@/shared/lib/utils';
+import BlogDataList from '@/features/Admin/Analytics/ui/BlogDataList';
+import { getBlogData } from '@/features/Admin/Analytics/hooks/getBlogData';
 
 export const metadata: Metadata = {
   title: 'Admin',
 };
-// 데이터 정의
-const adminData = [
-  { label: '포스트 수', value: '100 개' },
-  { label: '댓글 수', value: '121 개' },
-  { label: '총 조회 수', value: '121 회' },
-  { label: '인기포스트 조회수', value: '121 회' },
-  { label: '포스팅 수', value: '12 회' },
-];
-export default function AdminPage() {
+export default async function AdminPage() {
+  const data = await getBlogData();
+  const { adminData } = data;
   return (
     <div className="flex flex-wrap justify-start gap-4">
-      {adminData.map((item, index) => (
-        <Card
-          key={index}
-          className={cn(
-            'p-3 flex-grow w-full sm:w-[calc(50%-8px)] md:w-[calc(20%-16px)] lg:w-[calc(20%-16px)] xl:w-[calc(20%-16px)]',
-          )}
-        >
-          <CardContent className="flex items-center">
-            <div className="w-10 h-10 rounded-full bg-[#F4F7FE] flex items-center justify-center mr-3">
-              <img
-                src="/icons/admin_analytics.png"
-                alt={item.label}
-                className="w-6 h-6"
-              />
-            </div>
-            <div>
-              <div className="text-xs text-gray-400">{item.label}</div>
-              <div className="text-lg font-semibold">{item.value}</div>
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+      <BlogDataList adminData={adminData} />
     </div>
   );
 }
